3cb4c0c4b8dae6f9cc0cdfb0130d5b41495c6f75
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2021-07-23 16:51:48 +0100

    Create JavaScript.js

---
+++ HTML5 in 1 hour John Bura/3. More Shapes/JavaScript.js
@@ -0,0 +1,64 @@
+/* Not yet explained in Tutorial though assuming create variable,
+assign/attribute it to element in HTML by ID name */
+var jsCanvas = document.getElementById("jsCanvas");
+
+var context = jsCanvas.getContext("2d"); /* don't understand getContext() 
+                                      yet though again assuming just to 
+                                      designate type of element as
+                                      2D rather than 3D / other? */
+
+context.fillStyle = "darkmagenta"; /* set fill on var 'context' to red */
+
+context.fillRect(25,10,350,80); /* Create rectangle 
+`````````````````````````````````first two are x & y position,
+                                 last two are x & y size, 
+                                 orange rect behind appears due to
+                                 CSS on <canvas> tag*/
+
+
+var jsCanvas2 = document.getElementById("jsCanvas2");    
+var context2 = jsCanvas2.getContext("2d");
+var aGradient = context2.createRadialGradient(0,0,3,50,50,100);  
+/* create var - createRadialGradient(x0, y0, radius0, x1, y2, radius1) */
+
+aGradient.addColorStop(0, "cyan"); /* add colors into gradient */
+aGradient.addColorStop(1, "purple");
+
+context2.fillStyle = aGradient;  /* set fill to var aGradient */
+context2.fillRect(0,0,150,150);   /* set pos & size of rect */ 
+
+
+var jsCanvas3 = document.getElementById("jsCanvas3");
+var context3 = jsCanvas3.getContext("2d");
+var aGradient2 = context3.createLinearGradient(0,0,70,100,100);
+
+aGradient2.addColorStop(0, "black");
+aGradient2.addColorStop(1, "green");
+
+context3.shadowBlur = 30;
+context3.shadowColor = "black";
+
+context3.fillStyle = aGradient2; /* always add properties/attributes
+                                 above prior to setting fillStyle() &
+                                 fillRect() or it will draw them without
+                                 these properties included */
+context3.fillRect(0,0,150,150);
+
+/* the below isn't working yet and haven't figured out why... irritating */
+
+var jsCanvas4 = document.getElementById("jsCanvas4");
+var context4 = jsCanvas4.getContext("2d");
+
+context4.beginpath();
+context4.arc(50, 50, 50, 0, 2 * Math.PI);
+context4.stroke();
+
+var jsCanvas5 = document.getElementById*("jsCanvas5");
+var context5 = jsCanvas5.getContext("2d");
+
+context5.beginpath();
+context5.arc(50, 50 ,50, 0, 0.35 * Math.PI);
+context5.stroke();
+
+
+
