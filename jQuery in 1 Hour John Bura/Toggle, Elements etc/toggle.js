4228b06475d03866342ecdbc69994770a64c1ab9
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-10 21:01:49 +0000

    Create toggle.js

---
+++ jQuery in 1 Hour John Bura/Toggle, Elements etc/toggle.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $("h1").fadeOut("slow");
+    $("h2").fadeOut(4000);
+  });
+  
+  $(".otherButton").click(function(){
+    $(".fadeTag").fadeIn("fast");
+  });
+  
+  $("#toggleButton").click(function(){
+    $(".fadeTag").toggle();
+  });
+});
+
