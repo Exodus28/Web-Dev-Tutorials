5562e97b3349d80588e293d634111c2acfe28348
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-10 15:53:23 +0000

    Create fadeTiming.js

---
+++ jQuery in 1 Hour John Bura/Toggle, Elements etc/fadeTiming.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $(".fadeTag").fadeOut("slow");
+
+  });
+  
+  $(".otherButton").click(function(){
+    $(".fadeTag").fadeIn("fast");
+
+  });
+});
+
