fbeeacd87d5e6201b07656c793b540d1a20ab3ab
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-10 08:21:02 +0000

    Create adding jquery environment.js

---
+++ jQuery in 1 Hour John Bura/basics/adding jquery environment.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  
+});
+
+//above is more secure, below works too though
+//anything inside these would be jQuery, outside normal javascript
+
+$(function(){
+  
+});
