bc852e24bfe0d0cd62b96cb12e657aab6f1ef1be
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-14 13:25:42 +0000

    Create console_Variables_Test.js

---
+++ JavaScript&JQueryBasicsForBeginners/Intro, Variables & Primitives/console_Variables_Test.js
@@ -0,0 +1,22 @@
+var x=10
+undefined
+x
+10
+var y = 2
+undefined
+x + y
+12
+x * y 
+20
+x = x + 1
+11
+x
+11
+x = 1
+1
+X = 2
+2
+x + X
+3
+x + Y 
+VM1056:1 Uncaught ReferenceError: Y is not defined
