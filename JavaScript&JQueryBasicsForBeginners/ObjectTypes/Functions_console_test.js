3dafb987dcf25bd4df2f65aeafa3d2b5d8bb7fe8
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-15 10:49:58 +0000

    Create Functions_console_test.js

---
+++ JavaScript&JQueryBasicsForBeginners/ObjectTypes/Functions_console_test.js
@@ -0,0 +1,18 @@
+my_function()
+'hello'
+add(4, 2)
+6
+var f = add
+undefined
+//the function can be assigned to a variable / treated as an object
+undefined
+f(4, 6)
+10
+//as above, the variable now carries out the function
+undefined
+//functions can be created as variables without needing named e.g. ...
+undefined
+var multiply = function(a, b){ return a * b; }
+undefined
+multiply(2, 4)
+8
