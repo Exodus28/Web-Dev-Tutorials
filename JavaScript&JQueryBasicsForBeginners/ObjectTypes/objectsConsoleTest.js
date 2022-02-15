e70bc415c87354e72a955a68592688143e6c4f54
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-15 13:51:01 +0000

    Create objectsConsoleTest.js

---
+++ JavaScript&JQueryBasicsForBeginners/ObjectTypes/objectsConsoleTest.js
@@ -0,0 +1,14 @@
+bankAccount
+{owner: 'Ross', type: 'Savings', balance: 100, withdraw: ƒ}
+bankAccount.type
+'Savings'
+bankAccount.balance
+100
+bankAccount.withdraw
+ƒ (amount) {
+                                    this.balance -= amount;
+                             }
+bankAccount.withdraw(10)
+undefined
+bankAccount.balance
+90
