013fe9cad08756cd1644b0871362f6f52f562516
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-23 12:59:16 +0000

    Create WhileLoop.js

---
+++ JavascriptProgrammingComplete/WhileLoop.js
@@ -0,0 +1,18 @@
+var limit = 18;
+var age = 16;
+while (age <= limit) {
+    console.log("no alcohol for you!");
+    age++;
+} //not sure why they haven't included break; on this tutorial yet 
+
+//below for do while loop
+
+do {
+    alert("no alcohol for you!");
+    age++;
+}while (age <=limit);
+
+var k=1;
+while (k<15){
+    k++;
+}
