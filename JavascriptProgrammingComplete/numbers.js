165981b9652efb0a9f3cb98123734e6789806692
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-24 12:48:57 +0000

    Create numbers.js

---
+++ JavascriptProgrammingComplete/numbers.js
@@ -0,0 +1,18 @@
+var a=5;
+var b="6";
+var c="r";
+console.log(a+b);
+console.log(c*a);
+var newnumber=Number(b);
+console.log(newnumber);
+if(isNaN(c)) {
+    console.log("its not a number")
+}
+var x = 200.6;
+var y =Math.round(x);
+console.log(y);
+VM56:4 56
+VM56:5 NaN
+VM56:7 6
+VM56:9 its not a number
+VM56:13 201
