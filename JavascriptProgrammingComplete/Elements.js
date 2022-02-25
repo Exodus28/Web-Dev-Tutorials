a671e7824ddc59444bf8d1b853132911c47e32a0
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-02-25 16:12:16 +0000

    Add files via upload

---
+++ JavascriptProgrammingComplete/Elements.js
@@ -0,0 +1,14 @@
+var content = document.getElementById("paragraph2");
+content.setAttribute("align", "right");
+var test = document.getElementById("testing");
+console.log(testing.innerHTML);
+var list = document.getElementById("list");
+console.log(list.innerHTML);
+
+const newH = document.createElement("h1");
+const newP = document.createElement("p");
+newH.innerHTML="TipTopTitle";
+newP.innerHTML="PrimProperParagraph";
+document.getElementById("NewElement").appendChild(newH);
+document.getElementById("NewElement").appendChild(newP);
+
