a55274f0cfaa45a839af898813e103ddc297ca35
Chris Greaves <43829555+Exodus28@users.noreply.github.com>
2022-03-02 13:28:38 +0000

    Add files via upload

---
+++ JavascriptProgrammingComplete/Change CSS.js
@@ -0,0 +1,5 @@
+myElement.style.width="940px";
+myElement.style.paddingTop="-10px";
+myElement.style.margin="0 auto";
+myElement.style.fontWeight="bold";
+myElement.style.backgroundRepeat="repeat-x";
